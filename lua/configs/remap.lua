a54292ded5c5793bc7df8b9f751f6946a7053fe8
Sahitya Uniyal <sahityauniyal@gmail.com>
2025-01-07 14:50:50 +0000

    update

--- lua/configs/remap.lua
+++ lua/configs/remap.lua
@@ -1,5 +1,22 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
--- neotree
-vim.keymap.set("n","<C-n>", ":Neotree filesystem reveal left<CR>")
+-- movement
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- move line up and down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- paste something into the selected area without overwriting the default register
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- YANK
+-- yank to the selected text to clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+-- yank to the complete line to clipboard
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- delete without placing in register
+vim.keymap.set({ "n", "v" }, "<leader>d", '"_d')
