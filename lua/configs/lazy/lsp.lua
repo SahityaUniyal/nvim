ead421a3aa497941345b1713b6d4bbae2c13402b
Sahitya Uniyal <sahityauniyal@gmail.com>
2025-03-08 19:16:30 +0000

    add python config

--- lua/configs/lazy/lsp.lua
+++ lua/configs/lazy/lsp.lua
@@ -1,36 +1,38 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "gopls" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local lspconfig = require("lspconfig")
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls", "gopls", "pyright" },
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
 
-			lspconfig.lua_ls.setup({})
-			lspconfig.gopls.setup({})
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { noremap = true })
-			vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, { noremap = true })
+            lspconfig.lua_ls.setup({})
+            lspconfig.gopls.setup({})
+            lspconfig.pyright.setup({})
 
-			-- Configure diagnostics for floating window
-			vim.keymap.set("n", "<leader>dw", function()
-				vim.diagnostic.open_float(nil, { focus = false })
-			end, {})
-		end,
-	},
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+            vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { noremap = true })
+            vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, { noremap = true })
+
+            -- Configure diagnostics for floating window
+            vim.keymap.set("n", "<leader>dw", function()
+                vim.diagnostic.open_float(nil, { focus = false })
+            end, {})
+        end,
+    },
 }
