cc03b00918825f1dba691235eec670f1179009d6
Sahitya Uniyal <sahityauniyal@gmail.com>
2025-03-14 15:40:03 +0000

    add color scheme

--- lua/configs/lazy/colors.lua
+++ lua/configs/lazy/colors.lua
@@ -1,11 +1,18 @@
 return {
     {
-        "catppuccin/nvim",  -- Lazy load Catppuccin theme
+        "catppuccin/nvim",           -- Lazy load Catppuccin theme
         name = "catppuccin",
-        priority = 1000,    -- High priority to load this theme first
+        priority = 1000,             -- High priority to load this theme first
         config = function()
-           vim.cmd.colorscheme("catppuccin")  -- Set the desired Catppuccin flavor, e.g., "latte", "macchiato", "mocha", "frappe"
-        end
+            vim.cmd.colorscheme("catppuccin") -- Set the desired Catppuccin flavor, e.g., "latte", "macchiato", "mocha", "frappe"
+        end,
     },
+    -- {
+    -- 	"rose-pine/neovim", -- Lazy load Catppuccin theme
+    -- 	name = "rose-pine",
+    -- 	priority = 2000, -- High priority to load this theme first
+    -- 	config = function()
+    -- 		vim.cmd.colorscheme("rose-pine") -- Set the desired Catppuccin flavor, e.g., "latte", "macchiato", "mocha", "frappe"
+    -- 	end,
+    -- },
 }
-
